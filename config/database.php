597cd3ceff312bdb0c0938340afa30397b222e18
KaushalBhatol <kaushal.bhatol@outlook.com>
2025-05-07 12:17:09 +0000

    updated database ip

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 ### DATABASE CONFIGURATION
-$GLOBALS['DBHOST'] = "10.50.80.58";
+$GLOBALS['DBHOST'] = "51.20.122.20";
 $GLOBALS['DBNAME'] = "supershop_rdb";
 $GLOBALS['DBUSER'] = "siteuser";
 $GLOBALS['DBPASS'] = "StrongPassword123!";

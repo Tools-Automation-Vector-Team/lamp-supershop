be449f19aa13586a3fff0900e4c5ccd71dc53480
KaushalBhatol <kaushal.bhatol@outlook.com>
2025-05-07 11:45:15 +0000

    added

---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?php
+### DATABASE CONFIGURATION
+$GLOBALS['DBHOST'] = "10.50.80.58";
+$GLOBALS['DBNAME'] = "supershop_rdb";
+$GLOBALS['DBUSER'] = "siteuser";
+$GLOBALS['DBPASS'] = "StrongPassword123!";
+?>
